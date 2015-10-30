a4ca5848234ab9a6ee72556e8ccd0de9fa31137f
enisbt <enisbtuysuz@gmail.com>
2015-10-30 13:18:37 +0300

    Improved performance and search results

--- Youtube_music_player.py
+++ Youtube_music_player.py
@@ -3,16 +3,11 @@ import webbrowser
 import urllib2
 print "-------------\n YOUTUBE MUSIC OPENER\n-------------"
 user_want=raw_input("What do you want me to play.\n")
-user_want=list(user_want)
-#Have to remove spaces because it gives error when program wants to view source code.
-for char in user_want:
-    if char==" ":
-        user_want.remove(char)
-user_want="".join(map(str,user_want))
-youtube_link="https://www.youtube.com/results?search_query="
-search_link=youtube_link+user_want
+#Replace spaces with + because YouTube searchs this way.
+user_want = user_want.replace(" ", "+")
+youtube_link="https://www.youtube.com/results?search_query=%s" % (user_want)
 #view source code
-request=urllib2.Request(search_link)
+request=urllib2.Request(youtube_link)
 response=urllib2.urlopen(request)
 ans=response.readlines()
 #find wacth?v= in source code then program open first video in search page
